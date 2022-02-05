29b335b0a9407c9f8df70c397714f77a1c5232de
Mathew Varghese <mathewvarghesemanu@gmail.com>
2022-02-05 11:51:11 -0800

    variable names changed

--- skill_automation.py
+++ skill_automation.py
@@ -4,8 +4,8 @@ import pyttsx3
 engine = pyttsx3.init()
 
 
-given="Numpy, Pandas, Matplotlib, Pytorch, Tensorflow, Keras, Scikit Learn, OpenCV, NLTK, Scikit Multiflow, Deep Learning, Generalized Linear Models, Dimensionality Reduction, Model Optimization, Neural Networks, Transformers, GANs, Ensemble Methods, Long-Tailed Recognition, Time Series Analysis, Python, Arduino, C++, SQL,Regex, AWS, Spark, Git, Flask, REST APIs, React, Flutter, Firebase, Docker, Kubernetes ,3D printing, rapid prototyping, digital fabrication, web designing, Design Thinking, product management, product prototyping, documentation, technical writing, leadership, presentation, intrapreneurship, entrepreneurship, people management, team player, initiative, proactiveness, automation, CNNs, python scripting, Autohotkey scripting"
-given_list=given.split(",")
+skills="Numpy, Pandas, Matplotlib, Pytorch, Tensorflow, Keras, Scikit Learn, OpenCV, NLTK, Scikit Multiflow, Deep Learning, Generalized Linear Models, Dimensionality Reduction, Model Optimization, Neural Networks, Transformers, GANs, Ensemble Methods, Long-Tailed Recognition, Time Series Analysis, Python, Arduino, C++, SQL,Regex, AWS, Spark, Git, Flask, REST APIs, React, Flutter, Firebase, Docker, Kubernetes ,3D printing, rapid prototyping, digital fabrication, web designing, Design Thinking, product management, product prototyping, documentation, technical writing, leadership, presentation, intrapreneurship, entrepreneurship, people management, team player, initiative, proactiveness, automation, CNNs, python scripting, Autohotkey scripting"
+given_list=skills.split(",")
 given_list=list(map(lambda a: a.strip(),given_list))
 
 time.sleep(2)
